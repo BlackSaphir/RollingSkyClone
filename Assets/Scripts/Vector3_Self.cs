35c037bb1dc7e69385a455e4fdcf40a839b3a216
Carsten <carsten.dollack@yahoo.de>
2016-11-10 09:04:11 +0100

    [ADDED] Vector3S and Material for Ball

---
+++ Assets/Scripts/Vector3_Self.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Vector3Self : MonoBehaviour
+{
+    public struct Vector3S
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3S(float x, float y)
+            : this()
+        {
+            U = x;
+            V = y;
+        }
+
+        public float U { get; private set; }
+        public float V { get; private set; }
+
+
+        public Vector3S(float x, float y, float z)
+            : this()
+        {
+            A = x;
+            B = y;
+            C = z;
+        }
+
+        public float A { get; private set; }
+        public float B { get; private set; }
+        public float C { get; private set; }
+
+
+        public  Vector3S back {get;} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
