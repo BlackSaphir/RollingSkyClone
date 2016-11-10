dab0b0c7df28b10860d23abef3a18ad0be5d3198
Carsten <carsten.dollack@yahoo.de>
2016-11-10 11:52:02 +0100

    [WORKED]on Vector3S

--- Assets/Scripts/Vector3_Self.cs
+++ Assets/Scripts/Vector3_Self.cs
@@ -15,9 +15,16 @@ public class Vector3Self : MonoBehaviour
             V = y;
         }
 
+        //erlaubt
         public float U { get; private set; }
         public float V { get; private set; }
 
+        //nicht erlaubt da property im Struct (gleicher typ)
+        //public Vector3S Penis { get; set; }
+
+        public float A { get; private set; }
+        public float B { get; private set; }
+        public float C { get; private set; }
 
         public Vector3S(float x, float y, float z)
             : this()
@@ -27,41 +34,40 @@ public class Vector3Self : MonoBehaviour
             C = z;
         }
 
-        public float A { get; private set; }
-        public float B { get; private set; }
-        public float C { get; private set; }
-
-
-        public  Vector3S back {get;} 
 
 
 
+        //public  Vector3S back {get;}
+        //prop Tab tab macht dir automatisch eine property in C# Easy 
+    }
+    public Vector3S back { get; set; }
+    public Vector3S down { get; set; }
+    public Vector3S forward { get; set; }
+    public Vector3S left { get; set; }
+    public Vector3S right { get; set; }
+    public Vector3S up { get; set; }
+    public float magnitude { get; set; }
+    public float normalize { get; set; }
+    public float sqrmagnitude { get; set; }
+    public static Vector3S operator +(Vector3S a, Vector3S b)
+    {
+
+    }
 
 
 
 
 
 
+    // Use this for initialization
+    void Start()
+    {
 
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
-
-
-
-
-
-
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
